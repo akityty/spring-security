4c12285c79a53676b0d029c925d537bb61556a0a
akityty <congnv.9803@gmail.com>
2019-10-15 12:01:33 +0700

    ok

---
+++ src/main/java/konkon/SecurityApplicatioInitializer.java
@@ -0,0 +1,7 @@
+package konkon;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityApplicatioInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
